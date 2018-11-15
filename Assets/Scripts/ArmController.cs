f3ecc86bfbed346b36966da464f0fd934c5195e4
mkp003 <mkp003@mail.usask.ca>
2018-11-15 17:36:56 -0600

    Added ArmController Script and scene.

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject handReference;
+
+    [SerializeField]
+    private GameObject armDisplay;
+
+    private bool isArmDisplayActive;
+
+    private float batteryLevel;
+
+    private float geigerCounterRate;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!isArmDisplayActive)
+        {
+            if (CheckToActivate())
+            {
+                ActivateArmGUI();
+            }
+        }
+        else
+        {
+            if (CheckToDeactivate())
+            {
+                DeactiveateArmGUI();
+            }
+        }
+	}
+
+    private bool CheckToActivate()
+    {
+        if(handReference.transform.rotation.z > 90.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool CheckToDeactivate()
+    {
+        if (handReference.transform.rotation.z < 70.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void ActivateArmGUI()
+    {
+        armDisplay.SetActive(true);
+        isArmDisplayActive = true;
+    }
+
+    private void DeactiveateArmGUI()
+    {
+        armDisplay.SetActive(false);
+        isArmDisplayActive = false;
+    }
+
+
+}
