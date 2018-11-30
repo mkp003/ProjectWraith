fe1be47e3d7fc5814147e728463aefa7986d8d3a
Mackenzie Power <mackenziepower@hotmail.com>
2018-11-30 11:06:36 -0600

    Changes to doors by Graeme.

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    [SerializeField]
+    GameObject doorOne;
+
+    [SerializeField]
+    GameObject doorTwo;
+
+    [SerializeField]
+    GameObject doorThree;
+
+    [SerializeField]
+    GameObject doorFour;
+
+    private Vector3 doorOneRestingLocation;
+
+    private Vector3 doorTwoRestingLocation;
+
+    private Vector3 doorThreeRestingLocation;
+
+    private Vector3 doorFourRestingLocation;
+
+
+    // Use this for initialization
+    void Start () {
+        doorOneRestingLocation = doorOne.transform.position;
+        doorTwoRestingLocation = doorTwo.transform.position;
+        doorThreeRestingLocation = doorThree.transform.position;
+        doorFourRestingLocation = doorFour.transform.position;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
