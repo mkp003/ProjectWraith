ba5c995a0799db10997a4ae6af5e897cbd9bba36
Mackenzie Power <mackenziepower@hotmail.com>
2019-04-13 14:50:30 -0600

    Added additional variable in pcg

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -75,11 +75,12 @@ public class MapGeneration : MonoBehaviour
     private int levellength = 150;
 
     private Section[,] levelArray;
+    private LinkedList<Section> listOfSections;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.levelArray = new Section[this.levelWidth,this.levellength];
+        //this.levelArray = new Section[this.levelWidth,this.levellength];
         AdjustDimensions();
         CreateLevel();
     }
@@ -104,17 +105,17 @@ public class MapGeneration : MonoBehaviour
 
     private void CreateLevel()
     {
-
+        CreateSections(0, this.levelWidth - 1, 0, this.levellength - 1);
     }
 
 
     /// <summary>
     /// CreateSections() Will create all the sections that will be in the array
     /// </summary>
-    /// <param name="xMin"></param>
-    /// <param name="xMax"></param>
-    /// <param name="zMin"></param>
-    /// <param name="zMax"></param>
+    /// <param name="xMin">starting position from the width of the room</param>
+    /// <param name="xMax">ending position from the width of the room</param>
+    /// <param name="zMin">starting position from the length of the room</param>
+    /// <param name="zMax">ending position from the length of the room</param>
     private void CreateSections(int xMin, int xMax, int zMin, int zMax)
     {
         // Check for minimum size
@@ -123,12 +124,21 @@ public class MapGeneration : MonoBehaviour
         if (currentWidth <= 10 && currentLength <= 10)
         {
             Section newSection = new Section(Random.Range(0, 1000), xMin, zMin, currentWidth, currentLength);
+            
         }
         else
         {
             if(currentWidth > currentLength)
             {
-                //CreateSections()
+                int randomPoint = Random.Range(xMin + 1, xMax);
+                CreateSections(xMin, randomPoint, zMin, zMax);
+                CreateSections(randomPoint + 1, xMax, zMin, zMax);
+            }
+            else
+            {
+                int randomPoint = Random.Range(zMin + 1, zMax);
+                CreateSections(xMin, xMin, zMin, randomPoint);
+                CreateSections(xMin, xMax, randomPoint + 1, zMax);
             }
         }
     }
